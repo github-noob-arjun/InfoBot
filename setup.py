acdfc80926e9cbf5ae0e4743870b67d479fb5b7f
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-21 17:02:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='IDbot',
-      version='0.1.9',
+      version='0.2.0',
       description='Python module for interacting with Chatbase APIs',
       url='https://github.com/github-noob-arjun/InfoBot',
       author='Github',
